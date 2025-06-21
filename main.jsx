700f50d8685f79eb3d7c0aa5ac9be73f535514c4
Nico14-ma <nicolasmolinos@gmail.com>
2025-06-21 18:08:59 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TracingPalabrasApp from "./TracingPalabrasApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TracingPalabrasApp />
+  </React.StrictMode>
+);
